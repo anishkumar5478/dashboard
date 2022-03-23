b3d95fd0ce46718aa9c22b1f17378dea30ec7aec
anishkumar5478 <anishkumar861999@gmail.com>
2022-03-23 10:57:02 +0530

    Commit

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,18 @@
 import React from "react";
-import { LineStyle, Timeline, TrendingUp } from "@mui/icons-material";
+import {
+  LineStyle,
+  Timeline,
+  TrendingUp,
+  PersonOutline,
+  Inventory,
+  Paid,
+  BarChart,
+  Mail,
+  Feedback,
+  Comment,
+  ManageAccounts,
+  Report,
+} from "@mui/icons-material";
 import "./sidebar.css";
 export default function Sidebar() {
   return (
@@ -19,6 +32,61 @@ export default function Sidebar() {
             <li className="sidebarListItem">
               <TrendingUp />
               Sale
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Quick Menu</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <PersonOutline />
+              Users
+            </li>
+            <li className="sidebarListItem">
+              <Inventory />
+              Products
+            </li>
+            <li className="sidebarListItem">
+              <Paid />
+              Transactions
+            </li>
+            <li className="sidebarListItem">
+              <BarChart />
+              Reports
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <Mail />
+              Mails
+            </li>
+            <li className="sidebarListItem">
+              <Feedback />
+              Feedback
+            </li>
+            <li className="sidebarListItem">
+              <Comment />
+              Messages
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <ManageAccounts />
+              Manage
+            </li>
+            <li className="sidebarListItem">
+              <Timeline />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <Report />
+              Reports
             </li>
           </ul>
         </div>
